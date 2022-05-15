e1409f02aa79ccd08edbe47be8d8d5423658a3da
Tino Wu <wuchaowei.tino@bytedance.com>
2022-05-15 20:07:10 +0800

    Initial Commit

---
+++ GDUTFeedBack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GDUTFeedBack
+//
+//  Created by Tino Wu on 2022/5/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
